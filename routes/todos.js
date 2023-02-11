afc7b27c2506096360d81dfcb48166ca3e3afda2
Shaheer Khan NS <khanshaheer43@gmail.com>
2023-02-11 13:31:21 +0530

    Typescript

---
+++ routes/todos.js
@@ -0,0 +1,55 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const router = (0, express_1.Router)();
+const todos = [];
+router.get("/todo", (req, res, next) => {
+    res.status(200).json({
+        todos,
+    });
+});
+router.post("/todo", (req, res, next) => {
+    const newTodo = {
+        id: new Date().toISOString(),
+        text: req.body.text,
+    };
+    todos.push(newTodo);
+    res.status(202).json({
+        status: "success",
+        data: todos,
+    });
+});
+router.delete("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    let index;
+    todos.forEach((item, i) => {
+        if (item.id === id) {
+            index = i;
+        }
+        if (index) {
+            todos.splice(index, 1);
+            res.status(202).json({
+                status: "success",
+            });
+        }
+        else {
+            res.status(404).json({
+                status: "fail",
+            });
+        }
+    });
+});
+router.patch("/todo/:id", (req, res) => {
+    const id = req.params.id;
+    const text = req.body.text;
+    todos.forEach((item) => {
+        if (item.id === id) {
+            item.text = text;
+            res.status(200).json({
+                status: "success",
+                data: todos,
+            });
+        }
+    });
+});
+exports.default = router;
---
+++ routes/todos.ts
@@ -0,0 +1,64 @@
+import { Router } from "express";
+
+import { Todo } from "../models/todos";
+
+const router = Router();
+
+const todos: Todo[] = [];
+
+router.get("/todo", (req, res, next) => {
+  res.status(200).json({
+    todos,
+  });
+});
+router.post("/todo", (req, res, next) => {
+  const newTodo: Todo = {
+    id: new Date().toISOString(),
+    text: req.body.text,
+  };
+
+  todos.push(newTodo);
+
+  res.status(202).json({
+    status: "success",
+    data: todos,
+  });
+});
+
+router.delete("/todo/:id", (req, res) => {
+  const id = req.params.id;
+  let index: number;
+  todos.forEach((item, i) => {
+    if (item.id === id) {
+      index = i;
+    }
+
+    if (index) {
+      todos.splice(index, 1);
+      res.status(202).json({
+        status: "success",
+      });
+    } else {
+      res.status(404).json({
+        status: "fail",
+      });
+    }
+  });
+});
+
+router.patch("/todo/:id", (req, res) => {
+  const id = req.params.id;
+  const text = req.body.text;
+
+  todos.forEach((item) => {
+    if (item.id === id) {
+      item.text = text;
+      res.status(200).json({
+        status: "success",
+        data: todos,
+      });
+    }
+  });
+});
+
+export default router;
